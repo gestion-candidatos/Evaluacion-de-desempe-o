2998a295ba44f175cddadae8138c5eee32fed828
FSFabric <florencia.salvaneschi@grupo-fabric.com>
2026-06-29 11:16:55 -0300

    Update PanelApp.jsx

--- src/components/PanelApp.jsx
+++ src/components/PanelApp.jsx
@@ -1011,12 +1011,7 @@ function PanelCalibracion({ cicloId, colabs, onHist, soloLectura }) {
  mapa[ev.colaborador_id].promLider = ev.rating_promedio;
  mapa[ev.colaborador_id].comentarioCalibracion = ev.comentario_calibracion || null;
  mapa[ev.colaborador_id].liderReabierto = reabiertos.has(ev.colaborador_id);
- var cal = ev.rating_calibrado;
- if (!cal && ev.rating_promedio) {
- cal = ev.rating_promedio;
- supabase.from('evaluaciones').update({ rating_calibrado: cal }).eq('id', ev.id);
- }
- mapa[ev.colaborador_id].ratingFinal = cal;
+  mapa[ev.colaborador_id].ratingFinal = ev.rating_calibrado || null;
  }
  });
  setDatos(Object.values(mapa)); setCarg(false);
